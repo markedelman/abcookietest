556679ef46f826a24241855af8bc65257f965d8b
Mark Edelman <markedelman1987@gmail.com>
2016-12-30 13:24:10 -0500

    updated everything from gomix

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
-<!-- Request and response drills
+Request and response drills
 ===========================
 
-Starter files for request and response drills assignment from Thinkful's Node course.
+Starter files for A/B test drill for Thinkful's Node course
 
-This app is set up with Express and runs in harmony mode, enabling ES6 features.
-
-
-https://github.com/Thinkful-Ed/hyperdev-req-resp-drills-starter-files -->
-
-Request and response drills
-
-Solution for redirection drill for Thinkful's Node course
-
-https://github.com/Thinkful-Ed/hyperdev-a-b-test-drill-solution
+https://github.com/Thinkful-Ed/hyperdev-a-b-test-drill-starter
--- server.js
+++ server.js
@@ -1,43 +1,27 @@
-// 'use strict';
-
-// // Request and response object drills
-// // ==================================
-
-// const express = require('express');
-// const app = express();
-
-// // your code here. 
-// // listen for requests :)
-// app.listen(process.env.PORT || 8080, () => console.log(
-//   `Your app is listening on port ${process.env.PORT || 8080}`));
-
 'use strict';
 
-// Solution to A/B test drill
-// ==========================
 
 const express = require('express');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const AB_COOKIE_NAME = 'a-b-test';
+const cookieParser = require('cookie-parser');
+
+const cookieName = 'abtest';
 
 app.use(cookieParser());
 app.use(express.static('public'));
 
+const assignAB = () => ['a','b'][Math.floor(Math.random()*2)];
 
-const assignAb = () => ['a', 'b'][Math.floor(Math.random() * 2)];
-
-app.get('/', (req, res) => {
-    const cookie = req.cookies[AB_COOKIE_NAME];
-    if (cookie === undefined) {
-        res.cookie(AB_COOKIE_NAME, assignAb(), {});
-    }
-    res.sendFile(__dirname + '/views/index.html');
-});
-
+app.get('/', (req, res)=> {
+  const cookie = req.cookies[cookieName];
+if (cookie === undefined) {
+  res.cookie(cookieName, assignAB(), {});
+}
+res.sendFile(__dirname + '/views/index.html')
+})
 
 // listen for requests :)
 app.listen(process.env.PORT || 8080, () => console.log(
-    `Your app is listening on port ${process.env.PORT || 8080}`));
+  `Your app is listening on port ${process.env.PORT || 8080 }`));
